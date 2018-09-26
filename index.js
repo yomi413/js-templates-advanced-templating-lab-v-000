865a772f32d98cf94a3459a59bca8eed44529560
Yomaira Escano <yjescano@gmail.com>
2018-09-26 02:43:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 })
 
 function createRecipe() {
-  let name = document.getElementByTagName("name").value;
+  let name = document.getElementById("name").value;
   let template = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
 }
 
