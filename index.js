34dd339b37333e535161773613161baa7bd68d3f
Yomaira Escano <yjescano@gmail.com>
2018-09-26 01:07:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function init() {
   Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
   function renderMain() {
     var template = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
-    var html = template({name: 'Gordon Ramsay'});
+    // var html = template({name: 'Gordon Ramsay'});
   }
 
   Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-details-partial").innerHTML)
