332ed804e09ed346192f2a00a9d04cad4942c501
Yomaira Escano <yjescano@gmail.com>
2018-09-26 00:04:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
+// Handlebars.registerHelper('displayIngredient', function() {
+//   return new Handlebars.SafeString(this.ingredient)
+// })
+
+// Handlebars.registerPartial('displayIngredient', document.getElementById("recipe-details-partial").innerHTML)
+// function renderMain() {
+//   var template = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
+//   var html = template({name: 'Gordon Ramsay'});
+// }
+
 function init() {
+  Handlebars.registerHelper('displayIngredient', function() {
+    return new Handlebars.SafeString(this.ingredient)
+  })
+
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
+  function renderMain() {
+    var template = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
+    var html = template({name: 'Gordon Ramsay'});
+  }
   //put any page initialization/handlebars initialization here
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+// var recipe = {
+//   description: 'yummy chicken noodle soup',
+//   ingredient: [
+//     {quantity: "1 cup", name: 'chicken'},
+//     {quantity: "3 nanoliters", name: 'stock'},
+//     {quantity: "12", name: 'noodles'}
+//   ]
+// }
+
+// var template = Handlebars.compile(document.getElementById("my-template").innerHTML);
+// var html = template(recipe);
